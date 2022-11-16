f8492d98f05ccb44c05934c56d4ec662457dda7f
Rodrigo <rodrigomsiqq@gmail.com>
2022-11-16 18:38:15 -0300

    adicionado laço for de contagem regressiva

--- LaçoFor.c
+++ LaçoFor.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
 int main(){
-    int x, y;
+    int x, y, contador;
     for(y = 0; y<=15; y++){
         printf("\n numeros=%d", y);
     }
 
-    
+
     //laço de repetição contando numeros pares
     for(x=0; x<=10; x+=2){
         printf("\n cont-numeros pares=%d", x);
+    }
+
+    //contagem regrassiva Laços
+    printf("\nDigite um número qualquer maior que 1: ");
+    scanf("%d", &contador);
+    for(contador; contador >=1; contador--){
+        printf("\n conte=%d", contador);
     }
     return 0;
 }
