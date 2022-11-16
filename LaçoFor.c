8890a53c7f470e3afd980b05b06f7610567c8335
Rodrigo <rodrigomsiqq@gmail.com>
2022-11-16 18:24:31 -0300

    adicionando studos laços de repetição -for-contando numeros pares

--- LaçoFor.c
+++ LaçoFor.c
@@ -4,5 +4,11 @@ int main(){
     for(y = 0; y<=15; y++){
         printf("\n numeros=%d", y);
     }
+
+    
+    //laço de repetição contando numeros pares
+    for(x=0; x<=10; x+=2){
+        printf("\n cont-numeros pares=%d", x);
+    }
     return 0;
 }
